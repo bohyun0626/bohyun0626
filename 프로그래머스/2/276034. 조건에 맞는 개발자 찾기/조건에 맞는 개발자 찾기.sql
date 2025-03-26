4a4c2e5f987203f916d206baa8f9421b42d9f8bf
보현 <bohyun0626@naver.com>
2025-03-26 20:08:10 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,13 @@
+-- 코드를 작성해주세요
+SELECT
+    DISTINCT id,
+    email,
+    first_name,
+    last_name
+FROM
+    developers d
+    JOIN skillcodes s
+    ON s.name IN ('C#', 'Python')
+    AND d.skill_code & s.code = s.code
+ORDER BY
+    1
